65ec2eae20f3d03c4e045dbf3b15fa6d9475c770
Spiderbuttons <azurylldmm@gmail.com>
2026-07-03 20:06:30 -0400

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Mono.Cecil;
+
+namespace StardewAssemblyNetwork;
+
+class Program
+{
+    public const string GAME_FOLDER = @"C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley";
+    
+    static void Main(string[] args)
+    {
+        DirectoryInfo gameFolder = new DirectoryInfo(GAME_FOLDER);
+        FileInfo? stardewAssembly = gameFolder.GetFiles("Stardew Valley.dll", SearchOption.AllDirectories).FirstOrDefault();
+        if (stardewAssembly == null)
+        {
+            Console.WriteLine("Stardew Valley.dll not found in the game folder.");
+            return;
+        }
+        
+        AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(stardewAssembly.FullName);
+
+        foreach (var type in assembly.MainModule.Types)
+        {
+            Console.WriteLine($"{type.FullName}");
+        }
+    }
+}
