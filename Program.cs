51904897a8d3b3cdafb3452362d43ec5baf0d703
Spiderbuttons <azurylldmm@gmail.com>
2026-07-04 19:17:59 -0400

    Somewhat functional stuff.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Mono.Cecil;
+﻿using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using Mono.Cecil;
 
 namespace StardewAssemblyNetwork;
 
@@ -20,7 +22,96 @@ class Program
 
         foreach (var type in assembly.MainModule.Types)
         {
-            Console.WriteLine($"{type.FullName}");
+            // if (type.Name is not "Bimap`2") continue;
+            if (type.Name.Contains("OverlayDictionary") == false) continue;
+            
+            if (type.Name is "<Module>") continue;
+            if (type.CustomAttributes.Any(attr => attr.AttributeType.FullName == "System.Runtime.CompilerServices.CompilerGeneratedAttribute")) continue;
+            
+            if (!type.HasGenericParameters) continue;
+
+            // foreach (var genParam in type.GenericParameters)
+            // {
+            //     Console.WriteLine(genParam.Name);
+            // }
+            
+            Console.WriteLine($"{type.FullNameNormalized()}");
+            
+            foreach (var member in type.Methods)
+            {
+                // if (member.Name is not "CopyTo") continue;
+                if (member.CustomAttributes.Any(attr => attr.AttributeType.FullName == "System.Runtime.CompilerServices.CompilerGeneratedAttribute")) continue;
+                
+                Console.WriteLine($"  {type.FullNameNormalized()}.{member.NameNormalized()}");
+            }
         }
+    }
+}
+
+public static class MemberExtensions
+{
+    public static string NameNormalized<T>(this T member) where T : IMemberDefinition, IGenericParameterProvider
+    {
+        string normalized = member.Name;
+
+        if (member.HasGenericParameters)
+        {
+            // TODO: Eventually I wanna see how much space is saved in the final json by removing the space in the delimiter.
+            string genericParams = string.Join(", ", member.GenericParameters.Select(p => p.Name));
+            normalized = $"{member.Name.Split('`')[0]}<{genericParams}>";
+        }
+
+        if (member is MethodDefinition method)
+        {
+            string parameters = string.Join(", ", method.Parameters.Select(p => $"{p.ParameterType.Resolve().NameNormalized()} {p.Name}"));
+
+            if (method.IsSpecialName)
+            {
+                if (method.IsConstructor)
+                {
+                    normalized = $"{member.DeclaringType.NameNormalized()}({parameters})";
+                } else if (member.Name is "set_Item" or "get_Item")
+                {
+                    normalized = $"[]";
+                } else if (member.Name.StartsWith("get_"))
+                {
+                    normalized = $"{member.Name.Substring(4)} {{ get; }}";
+                } else if (member.Name.StartsWith("set_"))
+                {
+                    normalized = $"{member.Name.Substring(4)} {{ set; }}";
+                }
+            } else normalized += $"({parameters})";
+        }
+        return normalized;
+    }
+    
+    public static string FullNameNormalized<T>(this T member) where T : IMemberDefinition, IGenericParameterProvider
+    {
+        // if (member.Name.Contains("KeyValuePair") && member is TypeReference type1)
+        // {
+        //     var type2 = type1.Resolve();
+        //     Console.WriteLine(type2.FullName);
+        //     Console.WriteLine(type2.DeclaringType is null ? "null" : type2.DeclaringType.Name);
+        //     Console.WriteLine(type2.HasGenericParameters ? string.Join(", ", type2.GenericParameters.Select(p => p.Name)) : "no generic params");
+        // }
+        
+        Stack<TypeDefinition> parentTypes = new Stack<TypeDefinition>();
+        TypeDefinition? currentType = member.DeclaringType;
+        while (currentType != null)
+        {
+            parentTypes.Push(currentType);
+            currentType = currentType.DeclaringType;
+        }
+        
+        string fullName = string.Join(".", parentTypes.Select(t => t.NameNormalized()));
+        if (!string.IsNullOrEmpty(fullName) && member.Name is not "set_Item" and not "get_Item") fullName += ".";
+        fullName += member.NameNormalized();
+
+        if (member is TypeReference type)
+        {
+            fullName = type.Namespace + (string.IsNullOrEmpty(type.Namespace) ? "" : ".") + fullName;
+        }
+        
+        return fullName;
     }
 }
